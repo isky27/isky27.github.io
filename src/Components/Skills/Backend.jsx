8bd8da47a4e714f37ad2cebd3e6c2ab53deb00d2
isky27 <gaganjindal50@gmail.com>
2022-10-26 17:50:32 +0530

    few updates

--- src/Components/Skills/Backend.jsx
+++ src/Components/Skills/Backend.jsx
@@ -123,8 +123,6 @@ function Backend() {
     ],
     [
       <svg
-        width="90px"
-        height="90px"
         viewBox="0 0 256 256"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
